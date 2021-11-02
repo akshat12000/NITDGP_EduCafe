68b770ba1bf7a2ae6de7beacb56fbb7e76b2a31e
Abantika Saha <abantika.saha18@gmail.com>
2021-11-02 22:56:18 +0530

    added routes

--- app.js
+++ app.js
@@ -12,169 +12,238 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret:"Our little Secret.",
-  resave:false,
+  secret: "Our little Secret.",
+  resave: false,
   saveUninitialized: false
 }));
 
 
-mongoose.connect('mongodb://localhost:27017/userDB',{useNewUrlParser:true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/studentDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const studentSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
     required: true
   },
-  email:{
+  email: {
     type: String,
     required: true
   },
-  contact:{
+  password:{
     type: String,
     required: true
   },
-  year:{
+  contact: {
     type: String,
     required: true
   },
-  roll:{
+  year: {
     type: String,
     required: true
   },
-  subjects:{
+  roll: {
     type: String,
-    "default" : [],
+    required: true
+  },
+  subjects: {
+    type: String,
+    "default": [],
     required: true
   }
 });
 
 const teacherSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
     required: true
   },
-  email:{
+  email: {
     type: String,
     required: true
   },
-  contact:{
+  password:{
     type: String,
     required: true
   },
-  subjects:{
+  contact: {
+    type: String,
+    required: true
+  },
+  subjects: {
     type: String,
     required: true
   }
 });
 
 const subjectSchema = new mongoose.Schema({
-  name:{
+  name: {
     type: String,
     required: true
   },
-  time:{
+  time: {
     type: String,
     required: true
   },
-  days:{
+  days: {
     type: String,
-    "default" : [],
+    "default": [],
     required: true
   }
 });
 
 const assignmentSchema = new mongoose.Schema({
-  teacherName:{
+  teacherName: {
     type: String,
     required: true
   },
-  subjectName:{
+  subjectName: {
     type: String,
     required: true
   },
-  question:{
+  question: {
     type: String,
     required: true
   },
-  deadline:{
-    time:{
-      startTime: Date,
-      endTime: Date,
+  deadline: {
+    startTime: {
+      type: Date,
+      required: true
     },
-    required: true
+    endTime: {
+      type: Date,
+      required: true
+    },
   }
 });
 
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  googleId: String,
-  secret: String
-});
+studentSchema.plugin(findOrCreate);
+teacherSchema.plugin(findOrCreate);
+subjectSchema.plugin(findOrCreate);
+assignmentSchema.plugin(findOrCreate);
 
-userSchema.plugin(findOrCreate);
+const Student = new mongoose.model("Student", studentSchema);
+const Teacher = new mongoose.model("Teacher", teacherSchema);
+const Subject = new mongoose.model("Subject", subjectSchema);
+const Assignment = new mongoose.model("Assignment", assignmentSchema);
 
-const User = new mongoose.model("User",userSchema);
 
-
-app.get("/",function(req,res){
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-
-app.get("/login",function(req,res){
-  res.render("login");
+app.get("/student", function (req, res) {
+  res.render("subhome", {
+    designation: "student",    
+  });
 });
 
-app.get("/register",function(req,res){
-  res.render("register");
+app.get("/teacher", function (req, res) {
+  res.render("subhome", {
+    designation: "teacher",    
+  });
 });
 
-app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      user = new User({
-        email:req.body.username,
-        password: hash
-      });
-      user.save(function(err){
-        if(err){
-          console.log(err);
-        }else{
-          res.render("secrets");
-        }
-      });
+
+app.get("/student/login", function (req, res) {
+  res.render("student_login");
+});
+
+app.get("/student/register", function (req, res) {
+  res.render("student_register");
+});
+
+app.get("/teacher/login", function (req, res) {
+  res.render("teacher_login");
+});
+
+app.get("/teacher/register", function (req, res) {
+  res.render("teacher_register");
+});
+
+app.post("/student/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    student = new Student({
+      email: req.body.username,
+      password: hash
+    });
+    student.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
     });
   });
+});
 
+app.post("/teacher/register", function (req, res) {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    teacher = new Tecaher({
+      email: req.body.username,
+      password: hash
+    });
+    teacher.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  });
+});
 
-app.get("/logout",function(req,res){
+app.get("/student/logout", function (req, res) {
   req.logout();
   res.redirect("/");
 });
 
-app.post("/login",function(req,res){
-  const username=req.body.username;
-  const password=req.body.password;
-  
-  User.findOne({email:username},function(err,foundUser){
-    if(err){
+app.get("/teacher/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/student/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  Student.findOne({ email: username }, function (err, foundStudent) {
+    if (err) {
       console.log(err);
-    }else{
-      if(foundUser){
-          bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result){
-              res.render("secrets");
-            }
+    } else {
+      if (foundStudent) {
+        bcrypt.compare(password, foundStudent.password, function (err, result) {
+          if (result) {
+            res.render("secrets");
+          }
         });
       }
     }
   });
 });
 
-app.listen(3000,function(req,res){
+app.post("/teacher/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  Teacher.findOne({ email: username }, function (err, foundTeacher) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundTeacher) {
+        bcrypt.compare(password, foundTeacher.password, function (err, result) {
+          if (result) {
+            res.render("secrets");
+          }
+        });
+      }
+    }
+  });
+});
+
+app.listen(3000, function (req, res) {
   console.log("The server is running at port 3000");
 });
